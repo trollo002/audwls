81169df8f1297cc3d19786df1c03c835fac4c097
trollo002 <76863924+trollo002@users.noreply.github.com>
2021-08-21 17:43:33 +0900

    Create 성적프로그램.c

---
+++ 성적프로그램.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int a, b, c;
+	a = 100;
+	b = 90;
+	c = 99;
+	int d;
+	d=a+b+c;
+	double e;
+	e = d / 3.0;
+	printf("총점 : %d\n평균 : %.1lf\n최고점수 : %d",d ,e, a);
+	
+}
